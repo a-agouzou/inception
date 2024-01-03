5ff263582e3844952b0d6257426dac78aa8da064
abdellah agouzoul <aagouzou1337@gmail.com>
2024-01-03 09:17:56 +0100

    add comments

--- srcs/wordpress/conf/script.sh
+++ srcs/wordpress/conf/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sleep 10
+
 wget -P /var/www https://wordpress.org/latest.tar.gz
 cd /var/www && tar -xzf /var/www/latest.tar.gz && rm /var/www/latest.tar.gz
 mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
@@ -11,6 +12,8 @@ wp config set --allow-root DB_USER ${WORDPRESS_DB_USER} --path='/var/www/wordpre
 wp config set --allow-root DB_PASSWORD ${WORDPRESS_DB_PASSWORD} --path='/var/www/wordpress'
 wp config set --allow-root DB_HOST "mariadb:3306" --path='/var/www/wordpress'
 wp user create --allow-root bob bob@example.com --role=author --user_pass="ma.aagouzou" --path='/var/www/wordpress'
-wp core install --allow-root --url="localhost" --title="aagouzou site" --admin_user="aagouzou" --admin_password="conima" --admin_email="admin@example.com" --path='/var/www/wordpress'
+wp core install --allow-root --url="localhost" --title="aagouzou site" --admin_user="aagouzou" --admin_password="conima" \
+    --admin_email="admin@example.com" --path='/var/www/wordpress'
 mkdir -p /run/php
+#used to start the PHP FastCGI Process Manager (PHP-FPM) in foreground
 /usr/sbin/php-fpm7.4 -F
