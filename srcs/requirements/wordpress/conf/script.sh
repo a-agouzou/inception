c4996837327f8edf75e48e2f084106f59a5093a2
abdellah agouzoul <aagouzou1337@gmail.com>
2024-01-05 18:22:27 +0100

    OK

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -2,8 +2,8 @@
 
 sleep 10
 
-wget -P /var/www https://wordpress.org/latest.tar.gz
-cd /var/www && tar -xzf /var/www/latest.tar.gz && rm /var/www/latest.tar.gz
+wget -P /var/www https://wordpress.org/wordpress-6.3.tar.gz
+cd /var/www && tar -xzf /var/www/wordpress-6.3.tar.gz && rm /var/www/wordpress-6.3.tar.gz
 mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 chown -R root:root /var/www/wordpress
 
@@ -16,7 +16,7 @@ wp config set --allow-root DB_NAME ${MYSQL_DB_NAME} --path='/var/www/wordpress'
 wp config set --allow-root DB_USER ${MYSQL_DB_USER} --path='/var/www/wordpress'
 wp config set --allow-root DB_PASSWORD ${MYSQL_DB_PASSWORD} --path='/var/www/wordpress'
 wp config set --allow-root DB_HOST "mariadb:3306" --path='/var/www/wordpress'
-wp core install --allow-root --url="aagouzou.42.fr" --title="aagouzou" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} \
+wp core install --allow-root --url="https://aagouzou.42.fr" --title="aagouzou" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} \
     --admin_email="aagouzou1337@gmail.com" --skip-email --path='/var/www/wordpress'
 wp user create --allow-root ${WP_USER_NAME} abdo@aagouzou.42.fr --role=author --user_pass=${WP_USER_PASS} --path='/var/www/wordpress'
 mkdir -p /run/php
